2965c73bdf35e537bbe4f45ec2c04900862ba425
nwxtima <timofey-rusakov@mail.ru>
2024-11-13 22:43:34 +0400

    Я упростил кнопку, она принимает пропс и выводит значение кнопки.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,7 @@
 type ButtonProps = {
     title: string
+
 }
-
-
-export const Button = (props:ButtonProps) =>{
-    return (
-       <button title={1}>{props.title}</button>
-       <button title={2}>{props.title}</button>
-       <button title={3}>{props.title}</button>
-
-    );
+export const Button = ({title}: ButtonProps) => {
+    return <button>{title}</button>
 }
